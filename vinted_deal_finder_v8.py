e84d5b6ee35c0e263133c1924aa40ca76013b18f
W1ther2 <ainius226@gmail.com>
2026-09-06 12:22:03 +0300

    Update vinted_deal_finder_v8.py

--- vinted_deal_finder_v8.py
+++ vinted_deal_finder_v8.py
@@ -38,7 +38,7 @@ DRY_RUN = False # Jei True - NESIUNCIA zinuciu i Telegram, tik issaugo ka jau ma
                # Pirmam paleidimui palik True, antram - pakeisk i False.
 
 # Saliu kodai, kuriu skelimu NENORI (pvz. "PL" = Lenkija).
-ALLOWED_COUNTRY_CODES = ["LT"]
+EXCLUDE_COUNTRY_CODES = ["PL"]
 
 # Jei True - atmeta skelbimus, kuriu tekste (pavadinimas/aprasymas) lenkiska kalba.
 FILTER_POLISH_TEXT = True
