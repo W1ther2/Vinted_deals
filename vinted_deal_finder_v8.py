6aacb7e3336e6f8dc40e2727ee1452f609d01bfe
W1ther2 <ainius226@gmail.com>
2026-09-06 10:39:34 +0300

    Update vinted_deal_finder_v8.py

--- vinted_deal_finder_v8.py
+++ vinted_deal_finder_v8.py
@@ -10,8 +10,8 @@ import time
 
 # ========== SUSIKONFIGUROK SITAS EILUTES ==========
 import os
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-CHAT_ID   = os.environ.get("CHAT_ID", "")
+BOT_TOKEN = os.environ.get("8841585835:AAFPdurK8vDhd6FTK53wqF8HnYt7Uou1kXA", "")
+CHAT_ID   = os.environ.get("6157710734", "")
 
 MODELS = [
     {"query": "iPhone 13 Pro", "min_price": 250, "max_price": 420},
