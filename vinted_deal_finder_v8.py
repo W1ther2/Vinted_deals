530fe70c6ea9b3e1bad466b52ad68103fda2406e
W1ther2 <ainius226@gmail.com>
2026-09-07 15:13:03 +0300

    Update vinted_deal_finder_v8.py

--- vinted_deal_finder_v8.py
+++ vinted_deal_finder_v8.py
@@ -19,8 +19,8 @@ CHAT_ID   = os.environ.get("CHAT_ID", "")
 MODELS = [
      {"query": "iPhone 13", "min_price": 100, "max_price": 160},
      {"query": "iPhone 13 Pro", "min_price": 100, "max_price": 200},
-     {"query": "iPhone 14", "min_price": 100, "max_price": 180},
-     {"query": "iPhone 14 Pro", "min_price": 100, "max_price": 300},
+     {"query": "iPhone 14", "min_price": 100, "max_price": 200},
+     {"query": "iPhone 14 Pro", "min_price": 100, "max_price": 350},
 ]
 
 BLACKLIST_WORDS = [
